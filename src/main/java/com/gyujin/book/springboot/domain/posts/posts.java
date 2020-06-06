f12ceb1a10e56df942ad09990933db7f65fd0b10
gyujin89 <gyujin89@gmail.com>
2020-06-06 20:58:06 +0900

    chapter3 JPA 초기 설정

---
+++ src/main/java/com/gyujin/book/springboot/domain/posts/posts.java
@@ -0,0 +1,37 @@
+package com.gyujin.book.springboot.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class posts {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(colunmDefinition = "TEXT" , nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+}
